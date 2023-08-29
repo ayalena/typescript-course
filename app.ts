f0b15d4343a5746ed794d93674af93862d96ae5a
Eline Hermans <eline.hermans@kidskonnect.nl>
2023-08-29 14:35:38 +0200

    tuples

--- app.ts
+++ app.ts
@@ -1,10 +1,17 @@
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Fenn",
   age: 10,
   hobbies: ["Draw", "Acro"],
+  role: [1, "Favorite Cuz"],
 };
 
-console.log(person.name);
+// person.role.push("Smarty Pants");
+console.log(person.role);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
