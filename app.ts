c773b96d171e8fb4b3ae027f04af61fc14142496
Eline Hermans <eline.hermans@kidskonnect.nl>
2023-09-21 15:21:54 +0200

    literal types p.2

--- app.ts
+++ app.ts
@@ -1,19 +1,18 @@
 function combine(
   input1: number | string,
   input2: number | string,
-  resultConversion: string
+  resultConversion: "as-number" | "as-text"
 ) {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
-  if (resultConversion === "as-number") {
-    return +result;
-  } else {
-    return result.toString();
-  }
+  return result;
 }
 
 const combinedAges = combine(30, 26, "as-number");
