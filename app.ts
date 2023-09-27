3d2115c4aa1088d1d736f684d1139dcd9445bb15
Eline Hermans <eline.hermans@kidskonnect.nl>
2023-09-27 12:10:06 +0200

    functions as types & callbacks

--- app.ts
+++ app.ts
@@ -16,3 +16,20 @@ function printUndefinedResult(num: number): undefined {
   console.log("Result: " + num);
   return;
 }
+
+let combineValues: (a: number, b: number) => number;
+combineValues = add;
+console.log(combineValues(9, 9));
+
+function addAndHandle(
+  n1: number,
+  n2: number,
+  callBackFunction: (num: number) => void
+) {
+  const result = n1 + n2;
+  callBackFunction(result);
+}
+
+addAndHandle(9, 11, (result) => {
+  console.log(result);
+});
