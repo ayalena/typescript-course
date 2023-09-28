17997d38d352856a429580031647cb8c3c38e714
Eline Hermans <eline.hermans@kidskonnect.nl>
2023-09-28 14:01:19 +0200

    the never type

--- app.ts
+++ app.ts
@@ -7,3 +7,9 @@ userInput = "Fenn";
 if (typeof userInput === "string") {
   userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
+}
+
+generateError("An error occurred!", 500);
