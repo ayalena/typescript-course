233d0e15854799f8597fc373e6f633cac09bfadc
Eline Hermans <eline.hermans@kidskonnect.nl>
2023-11-08 10:02:17 +0100

    getters & setters

--- src/app.ts
+++ src/app.ts
@@ -28,18 +28,39 @@ class ITDepartment extends Department {
 
 //with shorthand
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found.");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass a valid value");
+    }
+    this.addReport(value);
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
+    this.lastReport = reports[0];
   }
+
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+
   addEmployee(name: string) {
     if (name === "Charles") {
       return;
     }
     this.employees.push(name);
   }
-  addReport(text: string) {
-    this.reports.push(text);
-  }
+
   printReports() {
     console.log(this.reports);
   }
@@ -48,6 +69,9 @@ class AccountingDepartment extends Department {
 const it = new ITDepartment("D2", ["Elsie"]);
 
 const accounting = new AccountingDepartment("D1", []);
+
+// console.log(accounting.mostRecentReport);
+accounting.mostRecentReport = "Year end report";
 
 accounting.addEmployee("Fenn");
 
@@ -58,3 +82,4 @@ it.describe();
 
 accounting.addReport("Something went wrong");
 accounting.printReports();
+console.log(accounting.mostRecentReport);
